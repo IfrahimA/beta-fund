7636330f2f1303d766f918bb378422fe7c3a5b73
LJ <63890958+Gmaj9@users.noreply.github.com>
2024-12-12 18:32:22 -0500

    Added final two Reports (5 and 6; also made changes to tables and dummy data

--- backend/script.sql
+++ backend/script.sql
@@ -7,7 +7,12 @@ DROP TABLE IF EXISTS CLASSYEAR CASCADE;
 DROP TABLE IF EXISTS EMPLOYER CASCADE;
 DROP TABLE IF EXISTS DONATION CASCADE;
 DROP TABLE IF EXISTS PAYMENT CASCADE;
-DROP TABLE IF EXISTS DEFERREDPAYMENT;
+DROP TABLE IF EXISTS DEFERREDPAYMENT CASCADE;
+DROP TABLE IF EXISTS EVENTATTENDANCE CASCADE;
+DROP TABLE IF EXISTS CLASSCOORDINATOR CASCADE;
+DROP TABLE IF EXISTS VOLUNTEER CASCADE;
+DROP TABLE IF EXISTS VOLUNTEERASSIGNMENT CASCADE;
+DROP TABLE IF EXISTS PHONOTHON CASCADE;
 DROP TABLE IF EXISTS "event" CASCADE;
 
 -- Create CLASSYEAR table
@@ -15,6 +20,35 @@ CREATE TABLE CLASSYEAR (
     ClassID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     ClassYear INTEGER, 
 	CONSTRAINT ClassPK PRIMARY KEY (ClassID)
+);
+
+--Create CLASSCOORDINATOR table
+CREATE TABLE CLASSCOORDINATOR
+(
+    CoordinatorID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    FirstName TEXT,
+    LastName TEXT,
+    ClassID INTEGER,
+    CONSTRAINT CoordinatorPK PRIMARY KEY (CoordinatorID),
+    CONSTRAINT CoordinatorFK1 FOREIGN KEY (ClassID)
+    REFERENCES CLASSYEAR (ClassID)
+);
+
+--Create VOLUNTEER table
+CREATE TABLE VOLUNTEER
+(
+    VolunteerID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    FirstName TEXT,
+    LastName TEXT,
+    CONSTRAINT VolunteerPK PRIMARY KEY (VolunteerID)
+);
+
+--Create PHONOTHON table
+CREATE TABLE PHONOTHON
+(
+    PhonothonID INTEGER GENERATED ALWAYS AS IDENTITY,
+    PhonothonDate DATE NOT NULL,
+    CONSTRAINT PhonothonPK PRIMARY KEY (PhonothonID)
 );
 
 -- Create DONOR_CIRCLE table
@@ -60,6 +94,21 @@ CREATE TABLE DONOR (
 	REFERENCES DONOR_CIRCLE (CircleID)
 );
 
+--Create VOLUNTEERASSIGNMENT table
+CREATE TABLE VOLUNTEERASSIGNMENT
+(
+    VolunteerID INTEGER NOT NULL,
+    DonorID INTEGER NOT NULL,
+    PhonothonID INTEGER NOT NULL,
+    CONSTRAINT VolunteerAssignmentPK PRIMARY KEY (VolunteerID, DonorID, PhonothonID),
+    CONSTRAINT VolunteerAssignmentFK1 FOREIGN KEY (VolunteerID)
+    REFERENCES VOLUNTEER (VolunteerID),
+    CONSTRAINT VolunteerAssignmentFK2 FOREIGN KEY (DonorID)
+    REFERENCES DONOR (DonorID),
+    CONSTRAINT VolunteerAssignmentFK3 FOREIGN KEY (VolunteerID)
+    REFERENCES VOLUNTEER (VolunteerID)
+);
+
 -- Create LETTER table
 CREATE TABLE LETTER (
     LetterID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -77,10 +126,7 @@ CREATE TABLE "event" (
     EventName TEXT,
     EventDate DATE,
     EventLocation TEXT,
-    DonorID INTEGER,
-    CONSTRAINT EventPK PRIMARY KEY (EventID),
-    CONSTRAINT EventFK1 FOREIGN KEY (DonorID)
-    REFERENCES DONOR(DonorID)
+    CONSTRAINT EventPK PRIMARY KEY (EventID)
 );
 
 --Create EVENTATTENDANCE table
@@ -147,7 +193,38 @@ VALUES
     (2023),
     (2022),
     (2021),
-    (2020);
+    (2020),
+    (2019);
+
+--Insert data into CLASSCOORDINATOR table
+INSERT INTO CLASSCOORDINATOR (FirstName, LastName, ClassID)
+VALUES 
+    ('Emily', 'Davis', 1),
+    ('Michael', 'Brown', 2),
+    ('Sophia', 'Lee', 3),
+    ('William', 'Harris', 4),
+    ('Olivia', 'Martin', 5),
+    ('James', 'Wilson', 6);
+
+--Insert data into VOLUNTEER table
+INSERT INTO VOLUNTEER (FirstName, LastName)
+VALUES 
+    ('Ava', 'Garcia'),
+    ('Liam', 'Martin'),
+    ('Isabella', 'Hernandez'),
+    ('Noah', 'Diaz'),
+    ('Mia', 'Gonzalez'),
+    ('Ethan', 'Rodriguez');
+
+--Insert data into PHONOTHON table
+INSERT INTO PHONOTHON (PhonothonDate)
+VALUES 
+    ('2023-02-15'),
+    ('2022-03-10'),
+    ('2021-04-05'),
+    ('2020-05-01'),
+    ('2019-06-15'),
+    ('2018-07-10');
 
 -- Insert data into DONOR_CIRCLE table
 INSERT INTO DONOR_CIRCLE (CircleName, MinAmount, MaxAmount)
@@ -166,12 +243,27 @@ VALUES --'E' indicates that the backslash escape sequence is used
 -- Insert data into DONOR table
 INSERT INTO DONOR (FirstName, LastName, Street, City, State_, Zip, PhoneNumber, Email, Category, ClassID, CircleID) 
 VALUES 
-    ('John', 'Doe', 'Long Lane', 'Small Town', 'OK', '55599', '555-1234', 'john.doe@example.com', 'Alumni', 1, 1),
-    ('Jane', 'Smith', 'Station Avenue', 'Center Valley', 'PA', '18034', '555-5678', 'jane.smith@example.com', 'Parent', 2, 2),
-    ('Alice', 'Johnson', 'Wide Road', 'Redmond', 'WA', '88044', '555-8765', 'alice.johnson@example.com', 'Faculty', 3, 3),
-    ('Bob', 'Williams', 'Narrow Avenue', 'New York', 'NY', '23994', '555-4321', 'bob.williams@example.com', 'Student', 4, 4),
-    ('Charlie', 'Brown', 'Short Street', 'Brickville', 'OH', '22211', '555-6789', 'charlie.brown@example.com', 'Alumni', 5, 5),
-    ('Diana', 'Clark', 'Cherry Street', 'Sakura', 'CA', '13955', '555-9876', 'diana.clark@example.com', 'Other', 1, 6);
+    ('John', 'Doe', 'Long Lane', 'Small Town', 'OK', '55599', '123-555-1234', 'john.doe@example.com', 'Alumni', 1, 1),
+    ('Jane', 'Smith', 'Station Avenue', 'Center Valley', 'PA', '18034', '889-555-5678', 'jane.smith@example.com', 'Parent', NULL, 2),
+    ('Alice', 'Johnson', 'Wide Road', 'Redmond', 'WA', '88044', '703-555-8765', 'alice.johnson@example.com', 'Faculty', NULL, 3),
+    ('Bob', 'Williams', 'Narrow Avenue', 'New York', 'NY', '23994', '231-555-4321', 'bob.williams@example.com', 'Student', 4, 4),
+    ('Charlie', 'Brown', 'Short Street', 'Brickville', 'OH', '22211', '222-555-6789', 'charlie.brown@example.com', 'Alumni', 5, 5),
+    ('Stewart','Smith', 'Shady Lane', 'Arborville', 'NJ', '33918', '267-212-1212', 'ssmith@my.email', 'Alumni', 5, 10),
+    ('Diana', 'Clark', 'Cherry Street', 'Sakura', 'CA', '13955', '610-555-9876', 'diana.clark@example.com', 'Other', NULL, 6);
+
+INSERT INTO VOLUNTEERASSIGNMENT  (VolunteerID, DonorID, PhonothonID)
+VALUES
+    (3, 5, 2),
+    (3, 2, 2),
+    (2, 1, 1),
+    (4, 3, 2),
+    (1, 1, 3),
+    (2, 4, 4),
+    (2, 1, 3),
+    (4, 3, 5),
+    (2, 1, 2),
+    (5, 6, 1),
+    (1, 6, 2);
 
 -- Insert data into LETTER table
 INSERT INTO LETTER (LetterType, LetterDate, DonorID)
@@ -184,14 +276,14 @@ VALUES
     ('Appeal', '2024-01-20', 6);
 
 -- Insert data into "event" table
-INSERT INTO "event" (EventName, EventDate, EventLocation, DonorID)
+INSERT INTO "event" (EventName, EventDate, EventLocation)
 VALUES 
-    ('Annual Gala', '2024-05-10', 'Grand Ballroom', 1),
-    ('Homecoming', '2024-09-22', 'Campus Courtyard', 2),
-    ('Faculty Dinner', '2024-11-05', 'Faculty Lounge', 3),
-    ('Student Orientation', '2024-08-20', 'Student Center', 4),
-    ('Fundraising Banquet', '2024-10-30', 'Hilton Hotel', 5),
-    ('Alumni Meet & Greet', '2024-06-15', 'Alumni Hall', 6);
+    ('Annual Gala', '2024-05-10', 'Grand Ballroom'),
+    ('Homecoming', '2024-09-22', 'Campus Courtyard'),
+    ('Faculty Dinner', '2024-11-05', 'Faculty Lounge'),
+    ('Fall Carnival', '2024-08-20', 'University Park'),
+    ('Fundraising Banquet', '2024-10-30', 'Hilton Hotel'),
+    ('Alumni Meet & Greet', '2024-06-15', 'Alumni Hall');
 
 --Insert data into EVENTATTENDANCE table
 INSERT INTO EVENTATTENDANCE (EventID, DonorID)
@@ -207,6 +299,9 @@ VALUES
     (4, 2),
     (1, 3),
     (3, 1),
+    (5, 1),
+    (5, 6),
+    (3, 6),
     (5, 5);
 
 -- Insert data into DONATION table
@@ -221,7 +316,9 @@ VALUES
     (1500.00, '2024-03-01', TRUE, 3),
     (50.00, '2024-04-10', FALSE, 4),
     (5000.00, '2024-05-01', TRUE, 5),
-    (25000.00, '2024-06-10', TRUE, 6);
+    (25000.00, '2024-06-10', TRUE, 6),
+    (99999999.00, '2022-01-17', FALSE, 6),
+    (830.00, '2023-02-13', TRUE, 2);
 
 -- Insert data into EMPLOYER table
 INSERT INTO EMPLOYER (EmployerName, DonorID)
