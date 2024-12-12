b31cd9baa1acf26c170084e3653b6f6d6bc0116b
LJ <63890958+Gmaj9@users.noreply.github.com>
2024-12-12 02:55:50 -0500

    Add updated SQL files with Annual Report and its 6 summaries

--- backend/query.sql
+++ backend/query.sql
@@ -1,1 +1,243 @@
+--=====================================
+-- Queries for application reports
+-- (3 with input and 3 without input for 6 in total)
+--
+-- The reports are:
+-- 1) Annual Report to Donors (with 6 different summaries in addition
+--     to the main report - so, 7 queries are needed)
+-- 2) Monthly Report
+-- 3) Payments Due Report
+-- 4) Event Report (needs input to select the event)
+-- 5) Class Representative Contact List (needs input to select the class
+--     representative)
+-- 6) Phonothon Volunteer Contact List (needs input to select the volunteer)
+-- 
+--=====================================
+
+-- Query to get donors and donations
 SELECT * FROM donor JOIN donation USING(donorid);
+
+/* (Report 1) - Annual Report to Donors
+ *
+ * Gets the name, category, and donor circle for all donors in the current year
+ *
+ * (The project outline document says to list "all donors by category, year, and donor circle"
+ *  but that would get very long if not restricted to a few years at maximum.
+ *  So, it only gets donors for the current year)
+ */
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	donor.donorid,
+	donor.lastname,
+    donor.firstname,
+	category,
+	donor.circleid,
+	circlename
+FROM
+	donor,
+	donation,
+	donor_circle
+WHERE
+	(donor.donorid = donation.donorid)
+	AND (donor.circleid = donor_circle.circleid)
+	AND (
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	) --gets the current year and turns to CHAR for comparison
+ORDER BY
+	donor.lastname, donor.firstname ASC
+;
+
+/* Summaries for Annual Report
+ * 6 summaries:
+ * - 1) Total from all sources
+ * - 2) Total for each class
+ * - 3) Percent participation for each class
+ * - 4) Total for each donor category
+ * - 5) Grand total for each donor circle
+ * - 6) Class total for each donor circle
+ */
+
+-- (Annual Report Summary 1) Get the total raised from all donors
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	COUNT(DISTINCT (donor.donorid)) AS total_donors,
+	SUM(amount) AS total_raised_from_all_donors
+FROM
+	donor
+	INNER JOIN donation ON donor.donorid = donation.donorid
+WHERE
+	--gets the current year and turns to CHAR for comparison
+	to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+GROUP BY donation_year
+;
+
+-- (Annual Report Summary 2) Get the total raised per class for each class that had someone donate this year
+
+--Get only donors who have a class year, since some donors did not attend the university
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	classyear.classyear AS classyears_with_donors,
+	SUM(amount) AS total_raised_per_class
+FROM
+	donor,
+	donation,
+	classyear
+WHERE
+	(donor.donorid = donation.donorid)
+	AND (donor.classid = classyear.classid)
+	AND
+	--gets the current year and turns to CHAR for comparison
+	(
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	)
+GROUP BY
+	donation_year, classyear.classyear
+ORDER BY
+	classyear.classyear ASC
+;
+
+-- (Annual Report Summary 3) Get the percent participation per class year
+
+/* Select each class that was represented by donors this year and get the percentage of funds the class gave
+ * Select the sum of gift money from each class, and divide this by the sum of money received from all class years then multiply by 100 to get a percent
+ *  (It accounts for integer division by using '1.0 *')
+ * Make sure the percentage calculation only includes gifts from the current year!
+ */
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	classyear.classyear,
+	(
+		round((100 * (SUM(donation.amount)) / (
+				1.0 * (
+					SELECT
+						SUM(donation.amount)
+					FROM
+						donor,
+						donation
+					WHERE
+						(donor.donorid = donation.donorid)
+						AND --gets the current year and turns to CHAR for comparison
+						(
+							TO_CHAR(donation."Date", 'YYYY') = TO_CHAR(CURRENT_DATE, 'YYYY')
+						)
+				)
+			)
+		), 1)
+	) AS percent_participation_per_class
+FROM
+	donor,
+	donation,
+	classyear
+WHERE
+	(donor.donorid = donation.donorid)
+	AND (donor.classid = classyear.classid)
+	AND --gets the current year and turns to CHAR for comparison
+	(
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	)
+GROUP BY
+    donation_year,
+	classyear.classyear --aggregate on the class year
+ORDER BY
+	classyear.classyear ASC --start with the earliest classes
+;
+
+-- (Annual Report Summary 4) Get the total raised for each donor category that had donors this year.
+
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	donor.category AS donor_category,
+	SUM(amount) AS total_raised_per_category
+FROM
+	donor
+	INNER JOIN donation ON donor.donorid = donation.donorid
+WHERE
+	--gets the current year and turns to CHAR for comparison
+	(
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	)
+GROUP BY
+    donation_year,
+	donor.category --aggregate on donor category
+ORDER BY
+	donor.category ASC --sort the categories alphabetically
+;
+
+-- (Annual Report Summary 5) Grand total for each donor circle.
+
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	donor_circle.circleid,
+	donor_circle.circlename,
+	SUM(donation.amount) AS total_raised_per_donor_circle --sum the funds per circle
+FROM
+	donation,
+	donor_circle,
+	donor
+WHERE
+	(donor.donorid = donation.donorid)
+	AND (donor.circleid = donor_circle.circleid)
+	AND --gets the current year and turns to CHAR for comparison
+	(
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	)
+GROUP BY
+    donation_year,
+	donor_circle.circleid --aggregate on the donor circle recorded for each gift
+ORDER BY
+	donor_circle.circleid DESC
+;
+
+-- (Annual Report Summary 6) Class total for each donor circle.
+SELECT
+    /* The donation_year is useful for debugging to make sure only 
+     * donors from the current year are listed in the report.
+     */
+    to_char(donation."Date", 'YYYY') AS donation_year,
+	classyear.classid,
+	classyear.classyear,
+	donor_circle.circleid,
+	donor_circle.circlename,
+	SUM(donation.amount) AS class_total_per_donor_circle
+FROM
+	donor,
+	classyear,
+	donation,
+	donor_circle
+WHERE
+	(donor.donorid = donation.donorid)
+	AND (donor.classid = classyear.classid)
+	AND (donor.circleid = donor_circle.circleid)
+	AND --gets the current year and turns to CHAR for comparison
+	(
+		to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY')
+	)
+GROUP BY
+    donation_year,
+	classyear.classid,
+	classyear.classyear, --aggregate per class id and year
+	donor_circle.circleid,
+	donor_circle.circlename --and also aggregate per donor circle id and name
+ORDER BY
+	classyear.classyear DESC, --start with the oldest class years
+	donor_circle.circleid DESC --start with the highest donor circle per class year
+;
+
--- backend/script.sql
+++ backend/script.sql
@@ -9,88 +9,126 @@ DROP TABLE IF EXISTS DONATION CASCADE;
 DROP TABLE IF EXISTS PAYMENT CASCADE;
 DROP TABLE IF EXISTS "event" CASCADE;
 
--- Create DONOR table
-CREATE TABLE DONOR (
-    DonorID SERIAL PRIMARY KEY,
-    FirstName TEXT,
-    LastName TEXT,
-    PhoneNumber TEXT,
-    Email TEXT,
-    Category TEXT CHECK (Category IN ('Alumni', 'Parent', 'Family', 'Faculty', 'Student', 'Other'))
-);
-
--- Create LETTER table
-CREATE TABLE LETTER (
-    LetterID SERIAL PRIMARY KEY,
-    LetterType TEXT,
-    LetterDate TEXT,
-    DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+-- Create CLASSYEAR table
+CREATE TABLE CLASSYEAR (
+    ClassID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    ClassYear INTEGER, 
+	CONSTRAINT ClassPK PRIMARY KEY (ClassID)
 );
 
 -- Create DONOR_CIRCLE table
 CREATE TABLE DONOR_CIRCLE (
-    CircleID SERIAL PRIMARY KEY,
+    CircleID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     CircleName TEXT,
     MinAmount NUMERIC(10, 2),
     MaxAmount NUMERIC(10, 2),
-    DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+    CONSTRAINT CirclePK PRIMARY KEY (CircleID)
 );
 
--- Create "class" table
-CREATE TABLE CLASSYEAR (
-    ClassID SERIAL PRIMARY KEY,
-    Year INTEGER, 
+-- Create DONOR table
+CREATE TABLE DONOR (
+    DonorID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    FirstName TEXT,
+    LastName TEXT,
+    PhoneNumber TEXT,
+    Email TEXT,
+    Category TEXT CHECK (Category IN ('Alumni', 'Parent', 'Family', 'Faculty', 'Student', 'Other')),
+    ClassID INTEGER NULL,
+    CircleID INTEGER NULL,
+    CONSTRAINT DonorPK PRIMARY KEY (DonorID),
+    CONSTRAINT DonorFK1 FOREIGN KEY (ClassID)
+    REFERENCES CLASSYEAR(ClassID),
+    CONSTRAINT DonorFK2 FOREIGN KEY (CircleID)
+	REFERENCES DONOR_CIRCLE (CircleID)
+);
+
+-- Create LETTER table
+CREATE TABLE LETTER (
+    LetterID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    LetterType TEXT,
+    LetterDate TEXT,
     DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+    CONSTRAINT LetterPK PRIMARY KEY (LetterID),
+    CONSTRAINT LetterFK1 FOREIGN KEY (DonorID)
+    REFERENCES DONOR(DonorID)
 );
 
 -- Create "event" table
 CREATE TABLE "event" (
-    EventID SERIAL PRIMARY KEY,
+    EventID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     EventName TEXT,
     EventDate DATE,
     EventLocation TEXT,
     DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+    CONSTRAINT EventPK PRIMARY KEY (EventID),
+    CONSTRAINT EventFK1 FOREIGN KEY (DonorID)
+    REFERENCES DONOR(DonorID)
 );
 
 -- Create DONATION table
 CREATE TABLE DONATION (
-    DonationID SERIAL PRIMARY KEY,
+    DonationID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     Amount NUMERIC(10, 2),
     "Date" DATE,  -- Date field changed to DATE type
     MatchingGiftEligible BOOLEAN,
     DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+    CONSTRAINT DonationPK PRIMARY KEY (DonationID),
+    CONSTRAINT DonationFK1 FOREIGN KEY (DonorID)
+    REFERENCES DONOR(DonorID)
 );
 
 -- Create EMPLOYER table
 CREATE TABLE EMPLOYER (
-    EmployerID SERIAL PRIMARY KEY,
+    EmployerID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     EmployerName TEXT,
     DonorID INTEGER,
-    FOREIGN KEY (DonorID) REFERENCES DONOR(DonorID)
+    CONSTRAINT EmployerPK PRIMARY KEY (EmployerID),
+    CONSTRAINT EmployerFK1 FOREIGN KEY (DonorID)
+    REFERENCES DONOR(DonorID)
 );
 
 -- Create PAYMENT table
 CREATE TABLE PAYMENT (
-    PaymentID SERIAL PRIMARY KEY,
+    PaymentID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     PaymentMethod TEXT CHECK (PaymentMethod IN ('Check', 'Credit Card', 'Deferred')),
     DonationID INTEGER,
-    FOREIGN KEY (DonationID) REFERENCES DONATION(DonationID)
+    CONSTRAINT PaymentPK PRIMARY KEY (PaymentID),
+    CONSTRAINT PaymentFK1 FOREIGN KEY (DonationID)
+    REFERENCES DONATION(DonationID)
 );
 
--- Insert data into DONOR table
-INSERT INTO DONOR (FirstName, LastName, PhoneNumber, Email, Category) 
+-- Insert data into "class" table
+INSERT INTO CLASSYEAR (ClassYear)
 VALUES 
-    ('John', 'Doe', '555-1234', 'john.doe@example.com', 'Alumni'),
-    ('Jane', 'Smith', '555-5678', 'jane.smith@example.com', 'Parent'),
-    ('Alice', 'Johnson', '555-8765', 'alice.johnson@example.com', 'Faculty'),
-    ('Bob', 'Williams', '555-4321', 'bob.williams@example.com', 'Student'),
-    ('Charlie', 'Brown', '555-6789', 'charlie.brown@example.com', 'Alumni'),
-    ('Diana', 'Clark', '555-9876', 'diana.clark@example.com', 'Other');
+    (2024),
+    (2023),
+    (2022),
+    (2021),
+    (2020);
+
+-- Insert data into DONOR_CIRCLE table
+INSERT INTO DONOR_CIRCLE (CircleName, MinAmount, MaxAmount)
+VALUES --'E' indicates that the backslash escape sequence is used
+    (E'Friend\'s Circle', 100.00, 249.99),
+    ('Bronze Circle', 250.00, 499.99),
+    ('Silver Circle', 500.00, 999.99),
+    ('Gold Circle', 1000.00, 2499.99),
+    ('Grand Circle', 2500.00, 4999.99),
+    ('Elite Circle', 5000.00, 7499.99),
+    ('Ruby Circle', 7500.00, 14999.99),
+    ('Diamond Circle', 15000.00, 24999.99),
+    ('Platinum Circle', 25000.00, 49999.99),
+    (E'President\' Circle', 50000.00, 99999999.99);
+
+-- Insert data into DONOR table
+INSERT INTO DONOR (FirstName, LastName, PhoneNumber, Email, Category, ClassID, CircleID) 
+VALUES 
+    ('John', 'Doe', '555-1234', 'john.doe@example.com', 'Alumni', 1, 1),
+    ('Jane', 'Smith', '555-5678', 'jane.smith@example.com', 'Parent', 2, 2),
+    ('Alice', 'Johnson', '555-8765', 'alice.johnson@example.com', 'Faculty', 3, 3),
+    ('Bob', 'Williams', '555-4321', 'bob.williams@example.com', 'Student', 4, 4),
+    ('Charlie', 'Brown', '555-6789', 'charlie.brown@example.com', 'Alumni', 5, 5),
+    ('Diana', 'Clark', '555-9876', 'diana.clark@example.com', 'Other', 1, 6);
 
 -- Insert data into LETTER table
 INSERT INTO LETTER (LetterType, LetterDate, DonorID)
@@ -101,26 +139,6 @@ VALUES
     ('Reminder', '2024-01-18', 4),
     ('Thank You', '2024-01-19', 5),
     ('Appeal', '2024-01-20', 6);
-
--- Insert data into DONOR_CIRCLE table
-INSERT INTO DONOR_CIRCLE (CircleName, MinAmount, MaxAmount, DonorID)
-VALUES 
-    ('Silver Circle', 100.00, 499.99, 1),
-    ('Gold Circle', 500.00, 999.99, 2),
-    ('Platinum Circle', 1000.00, 4999.99, 3),
-    ('Diamond Circle', 5000.00, 9999.99, 4),
-    ('Elite Circle', 10000.00, 19999.99, 5),
-    ('Grand Circle', 20000.00, 50000.00, 6);
-
--- Insert data into "class" table
-INSERT INTO CLASSYEAR (Year, DonorID)
-VALUES 
-    (2024, 1),
-    (2023, 2),
-    (2022, 3),
-    (2021, 4),
-    (2020, 5),
-    (2024, 6);
 
 -- Insert data into "event" table
 INSERT INTO "event" (EventName, EventDate, EventLocation, DonorID)
