bbf9f900e9b0a6477ba28ea2e33b54e1dcb3ee06
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2024-12-12 19:09:01 -0500

    changing database to client instead of pool

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -12,11 +12,7 @@ const totalRaised = async () => {
 		const result = await client.query(
 			`SELECT to_char(donation."Date", 'YYYY') AS donation_year, COUNT(DISTINCT (donor.donorid)) AS total_donors, SUM(amount) AS total_raised_from_all_donors FROM donor INNER JOIN donation ON donor.donorid = donation.donorid WHERE to_char(donation."Date", 'YYYY') = to_char(CURRENT_DATE, 'YYYY') GROUP BY donation_year`
 		);
-
-		console.log(result);
-
 		const data = result.rows;
-		console.log('Fetched data:', data);
 		client.release();
 		return data;
 	} catch (error) {
@@ -26,7 +22,6 @@ const totalRaised = async () => {
 
 export default async function Page() {
 	const data = await totalRaised();
-	console.log(data[0].total_raised_from_all_donors);
 	const percentage = 90;
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen p-6'>
